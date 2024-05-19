07b1e6b88d98432705dcd310fede43eceb6c14df
TuanND <tuannd1058@gmail.com>
2024-05-19 18:49:59 +0700

    coding: add test case: can add the star name and star symbol properly

--- test/TestStarNotary.js
+++ test/TestStarNotary.js
@@ -80,6 +80,11 @@ it('lets user2 buy a star and decreases its balance in ether', async() => {
 it('can add the star name and star symbol properly', async() => {
     // 1. create a Star with different tokenId
     //2. Call the name and symbol properties in your Smart Contract and compare with the name and symbol provided
+    let instance = await StarNotary.deployed();
+    let name = await instance.name();
+    let symbol = await instance.symbol();
+    assert.equal(name, "HelloWorld");
+    assert.equal(symbol, "HLW");
 });
 
 it('lets 2 users exchange stars', async() => {
