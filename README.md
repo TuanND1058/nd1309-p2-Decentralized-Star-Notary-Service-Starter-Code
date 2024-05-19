05bfbb18db4bca8be5e02b16ca43dc575779ba47
TuanND <tuannd1058@gmail.com>
2024-05-19 20:15:08 +0700

    coding: deploy to test network sepolia

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ Truffle:        v5.11.5 (core: 5.11.5)
 OpenZeppelin:   2.3
 
 # Token Address
-Token Address:
+Token Address: fe8ca2feaf9a454d9f9acbd9cc7271ba
--- truffle-config.js
+++ truffle-config.js
@@ -22,11 +22,11 @@
  *
  */
 
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWallet = require('truffle-hdwallet-provider');
+const infuraKey = "fe8ca2feaf9a454d9f9acbd9cc7271ba";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -79,6 +79,12 @@ module.exports = {
       // network_id: 2111,   // This network is yours, in the cloud.
       // production: true    // Treats this network as if it was a public net. (default: false)
     // }
+    sepolia: {
+      provider: () => new HDWallet(mnemonic, `https://sepolia.infura.io/v3/${infuraKey}`),
+      network_id: 11155111,
+      gas: 5500000,
+      gasPrice: 10000000000
+    },
   },
 
   // Set default mocha options here, use special reporters etc.
