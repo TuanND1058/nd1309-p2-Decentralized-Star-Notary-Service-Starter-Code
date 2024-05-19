a78eed27c7a6a28a11158b3ddf8682681941ee17
TuanND <tuannd1058@gmail.com>
2024-05-19 19:05:56 +0700

    coding: add test case: lets a user transfer a star

--- test/TestStarNotary.js
+++ test/TestStarNotary.js
@@ -107,6 +107,13 @@ it('lets a user transfer a star', async() => {
     // 1. create a Star with different tokenId
     // 2. use the transferStar function implemented in the Smart Contract
     // 3. Verify the star owner changed.
+    let instance = await StarNotary.deployed();
+    let user1 = accounts[1];
+    let user2 = accounts[2];
+    let starId = 8;
+    await instance.createStar('Star to Transfer', starId, {from: user1});
+    await instance.transferStar(user2, starId, {from: user1});
+    assert.equal(await instance.ownerOf(starId), user2);
 });
 
 it('lookUptokenIdToStarInfo test', async() => {
